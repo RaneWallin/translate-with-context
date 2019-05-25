252e012cb4cc212e7a4ac43d63ec181b44474680
RaneWallin <rane@ranewallin.com>
2019-05-25 10:09:01 -0700

    refactor Button to use ColorContext

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 
 import LanguageContext from "../context/LanguageContext";
+import ColorContext from "../context/ColorContext";
 
 class Button extends Component {
   render() {
     return (
-      <button className="ui button primary">
-        <LanguageContext.Consumer>
-          {value => (value === "english" ? "Submit" : "Voorleggen")}
-        </LanguageContext.Consumer>
-      </button>
+      <ColorContext.Consumer>
+        {color => (
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {value => (value === "english" ? "Submit" : "Voorleggen")}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
     );
   }
 }
